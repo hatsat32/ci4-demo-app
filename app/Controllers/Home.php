2b3b68f52a4c9b031a5d0dcd29582c04ec81e8ce
Süleyman ERGEN <hatsat32@protonmail.com>
2021-06-13 19:45:24 +0300

    simple issue demo

---
+++ app/Controllers/Home.php
@@ -0,0 +1,32 @@
+<?php namespace App\Controllers;
+
+use CodeIgniter\Config\Factories;
+use \Myth\Auth\Models\UserModel as MythUserModel;
+use App\Models\UserModeld as AppUserModel;
+
+class Home extends BaseController
+{
+	public function index()
+	{
+		echo("Class Names");
+		d(MythUserModel::class);
+		d(AppUserModel::class);
+
+
+		echo("Models with helper");
+		d(model(MythUserModel::class));
+		d(model(AppUserModel::class));
+
+
+		echo("Models with factories");
+		d(Factories::models(MythUserModel::class));
+		d(Factories::models(AppUserModel::class));
+
+		echo("Models with factories (str)");
+		d(Factories::models('\App\Models\UserModel'));
+		d(Factories::models('\Myth\Auth\Models\UserModel'));
+
+		dd();
+		// return view('welcome_message');
+	}
+}
---
+++ app/Models/UserModel.php
@@ -0,0 +1,19 @@
+<?php namespace App\Models;
+
+use CodeIgniter\Model;
+
+class UserModel extends Model
+{
+    protected $table = 'users';
+    protected $primaryKey = 'id';
+
+    protected $allowedFields = [
+        'name', 'email', 'username', 'password_hash', 'deleted_at',
+    ];
+
+    protected $validationRules = [
+        'email'         => 'required|valid_email|is_unique[users.email,id,{id}]',
+        'username'      => 'required|alpha_numeric_punct|min_length[3]|max_length[30]|is_unique[users.username,id,{id}]',
+        'password_hash' => 'required',
+    ];
+}
